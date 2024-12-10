684d6c8b633ebd2028a618bf0b636f2273349698
kunal latta <95677039+KUNALLATTA29@users.noreply.github.com>
2024-12-10 16:24:25 +0530

    Update pract.jsx

--- pract.jsx
+++ pract.jsx
@@ -2,21 +2,40 @@ import "./styles.css";
 import { useState } from "react";
 
 export default function App() {
-  const [color, setcolor] = useState("yellow");
+  const [color, setcolor] = useState({
+    button1: "yellow",
+    button2: "yellow",
+  });
 
-  const handleclick = () => {
-    setcolor("green");
+  const handleclick = (button) => {
+    setcolor((prev) => ({
+      ...prev,
+      [button]: "green",
+    }));
     setTimeout(() => {
-      setcolor("yellow");
+      setcolor((prev) => ({
+        ...prev,
+        [button]: "yellow",
+      }));
     }, 4000);
   };
+
   return (
     <div>
       <button
         className="App"
-        style={{ backgroundColor: `${color}` }}
-        onClick={handleclick}
-      ></button>
+        style={{ backgroundColor: `${color.button1}` }}
+        onClick={() => handleclick("button1")}
+      >
+        1
+      </button>
+      <button
+        className="App"
+        style={{ backgroundColor: `${color.button2}` }}
+        onClick={() => handleclick("button2")}
+      >
+        2
+      </button>
     </div>
   );
 }
